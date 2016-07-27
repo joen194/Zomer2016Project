f7c534a8affbd02a1f9a827de974abbdc85d6b57
Jeroen <jeroenvanwolput1994@outlook.com>
2016-07-27 20:05:21 +0200

    Boek Kaarten toegevoegd

---
+++ Card-Games/Card-Games/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Games
+{
+    class Card
+    {
+        public enum Suits
+        {
+            Diamond,
+            Heart,
+            Spade,
+            Club
+        }
+
+        public enum Value
+        {
+            Two = 2, Three, Fore, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+        }
+
+        public Suits mySuit { get; set; }
+        public Value myValue { get; set; }
+    }
+}
---
+++ Card-Games/Card-Games/DeckOfCards.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Games
+{
+    class DeckOfCards : Card
+    {
+        const int MaxCards = 52; //maximum van kaarten
+        private Card[] deck; //dit is de verzameling van alle kaarten
+
+        public DeckOfCards() //de constuctor 
+        {
+            deck = new Card[MaxCards];
+        }
+
+        public Card[] getDeck { get { return deck; } } //voor in de andere klasse te plaatsen
+
+        public void setDeck()
+        {
+            int i = 0;
+            foreach (Suits s in Enum.GetValues(typeof(Suits)))
+            {
+                foreach (Value v in Enum.GetValues(typeof(Value)))
+                {
+                    deck[i] = new Card { mySuit = s, myValue = v };
+                    i++;
+                }
+            }
+            shuffle();
+        }
+
+        //kaarten schudden
+        public void shuffle()
+        {
+            Random rand = new Random();
+            Card temp;
+
+            // 100 keer schudden
+            for (int shuffle100 = 0; shuffle100 < 100; shuffle100++)
+            {
+                for (int i = 0; i < MaxCards; i++)
+                {
+                    int secondCardIndex = rand.Next(13); //Want er zijn 13 kaarten voor elke suit
+                    temp = deck[i];
+                    deck[i] = deck[secondCardIndex];
+                    deck[secondCardIndex] = temp;
+                }
+            }
+        }
+    }
+}
